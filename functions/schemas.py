277902106f9a88481093914f19c2078ed70cac62
Bailey Stringer <bailey4770@protonmail.com>
2025-11-27 19:43:14 +0800

    ai can now call every function

--- functions/schemas.py
+++ functions/schemas.py
@@ -15,8 +15,57 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Cat the contents of a specific file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to cat the content from, relative to the working directory. If not provided, returns an error.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a specific python file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The python file to execute, relative to the working directory. If not provided, returns an error.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes to a file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to write to, relative to the working directory. If not provided, returns an error.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The contents to write to the file. If not provided, returns an error.",
+            ),
+        },
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from google import genai
 from google.genai import types
 
 from functions.schemas import available_functions
-
+from functions.utils import (
+    get_files_info,
+    get_file_content,
+    write_file,
+    run_python_file,
+)
 
 MODEL = "gemini-2.0-flash-001"
 SYSTEM_PROMPT = """
@@ -14,9 +19,13 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
+WORKING_DIRTORY = "./calculator"
 
 
 def initialise_client():
@@ -53,6 +62,47 @@ def print_token_counts(response):
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    func_dict = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+
+    if function_call_part.name not in func_dict:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    func = func_dict[function_name]
+    function_result = func(**function_args, working_directory=WORKING_DIRTORY)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+
 def print_response(response):
     function_calls = response.function_calls
 
@@ -81,12 +131,20 @@ def main():
         ),
     )
 
-    if verbose:
-        print(f"User prompt: {prompt}")
-        print_response(response)
-        print_token_counts(response)
-    else:
-        print_response(response)
+    function_calls = response.function_calls
+    function_calls_responses = []
+
+    if function_calls:
+        for function_call_part in function_calls:
+            function_call_result = call_function(function_call_part, verbose)
+
+            if not function_call_result.parts[0].function_response.response:
+                raise RuntimeError("Something went fatally wrong")
+            else:
+                function_calls_responses.append(function_call_result.parts[0])
+
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
 
 
 if __name__ == "__main__":
